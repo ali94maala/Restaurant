c2e98d057075f8e3fcc702c8fb4d9d8d42c92132
ali94maala <99871296+ali94maala@users.noreply.github.com>
2022-07-13 11:11:46 +0400

    Create models (category & order) closes #5

---
+++ back-end/src/main/java/com/spring/restaurant/model/Category.java
@@ -0,0 +1,7 @@
+package com.spring.restaurant.model;
+
+public class Category {
+
+    private long id;
+    private String categoryName;
+}
---
+++ back-end/src/main/java/com/spring/restaurant/model/Order.java
@@ -0,0 +1,10 @@
+package com.spring.restaurant.model;
+
+public class Order {
+
+    private long id;
+    private String orderName;
+    private int price;
+    private String img;
+    private String description;
+}
