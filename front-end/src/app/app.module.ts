3c13d9b053f822bbd275da4994a122a0cb5ad7ae
ali94maala <99871296+ali94maala@users.noreply.github.com>
2022-07-14 17:02:51 +0400

    HttpClientModule & @CrossOrigin #26

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { OrderItemsComponent } from './componants/order-items/order-items.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,9 +11,12 @@ import { OrderItemsComponent } from './componants/order-items/order-items.compon
     OrderItemsComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
