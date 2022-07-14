c151418b23f9faeac63b4c3108ed3fe69c78f841
ali94maala <99871296+ali94maala@users.noreply.github.com>
2022-07-14 11:39:34 +0400

    getAllCategories (controller , service , Repository) closes #19

--- back-end/src/main/java/com/spring/restaurant/controller/CategoryController.java
+++ back-end/src/main/java/com/spring/restaurant/controller/CategoryController.java
@@ -1,9 +1,13 @@
 package com.spring.restaurant.controller;
 
 
+import com.spring.restaurant.model.Category;
 import com.spring.restaurant.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CategoryController {
@@ -13,5 +17,10 @@ public class CategoryController {
     @Autowired
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    @GetMapping("/api/allcategories")
+    public List<Category> getAllCategories(){
+        return categoryService.allCategories();
     }
 }
