8b7ec636dba05d4407f569b0a922bcbc1ccb9259
ali94maala <99871296+ali94maala@users.noreply.github.com>
2022-07-14 14:35:29 +0400

    Angular create OrderService closes #23

---
+++ front-end/src/app/service/order-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+import {Order} from "../model/order";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderServiceService {
+
+
+  private baseUrl = 'http://localhost:8080/api/allorders'
+  constructor( private http : HttpClient) { }
+
+  getOrder(): Observable<Order[]> {
+    return this.http.get<Order[]>(this.baseUrl).pipe(
+      map(
+        response => response
+      )
+    )
+  }
+}
