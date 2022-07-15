d0e98715cf22fe62d3e030691536952025814fff
ali94maala <99871296+ali94maala@users.noreply.github.com>
2022-07-15 10:35:35 +0400

    show image in order closes #27

--- front-end/src/app/model/order.ts
+++ front-end/src/app/model/order.ts
@@ -5,16 +5,16 @@ export class Order {
   description: string;
   data_create: Date;
   data_update: Date;
-  image: string;
+  img: string;
   price: number;
 
-  constructor(id: number, name: string, description: string, data_create: Date, data_update: Date, image: string, price: number) {
+  constructor(id: number, name: string, description: string, data_create: Date, data_update: Date, img: string, price: number) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.data_create = data_create;
     this.data_update = data_update;
-    this.image = image;
+    this.img = img;
     this.price = price;
   }
 
