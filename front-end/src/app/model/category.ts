1d9de506b92b331fe83fb1518472b8ea4a0af946
ali94maala <99871296+ali94maala@users.noreply.github.com>
2022-07-15 15:01:50 +0400

    Logos for category closes #33

--- front-end/src/app/model/category.ts
+++ front-end/src/app/model/category.ts
@@ -4,12 +4,14 @@ export class Category {
   name: string;
   data_create: Date;
   data_update:Date;
+  logo:string;
 
 
-  constructor(id: number, name: string, data_create: Date, data_update: Date) {
+  constructor(id: number, name: string, data_create: Date, data_update: Date, logo: string) {
     this.id = id;
     this.name = name;
     this.data_create = data_create;
     this.data_update = data_update;
+    this.logo = logo;
   }
 }
