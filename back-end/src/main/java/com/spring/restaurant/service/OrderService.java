d6b259fc0690c789857f3f9233031b91bd4bdd96
ali94maala <99871296+ali94maala@users.noreply.github.com>
2022-07-16 14:30:35 +0400

    Orders find by id category (Reapository & service) #37

--- back-end/src/main/java/com/spring/restaurant/service/OrderService.java
+++ back-end/src/main/java/com/spring/restaurant/service/OrderService.java
@@ -22,4 +22,8 @@ public class OrderService {
     public List<Order> getAllOrders(){
         return orderRepository.findAll();
     }
+
+    public List<Order> getOrderByIdCategories(Long id){
+        return orderRepository.findByCategoryId(id);
+    }
 }
