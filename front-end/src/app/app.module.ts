e93e5bed225bab68abbc8d2630d57ae9cb83ad92
ali94maala <99871296+ali94maala@users.noreply.github.com>
2022-07-19 16:56:22 +0400

    Testing closes #65

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { OrderItemsComponent } from './componants/order-items/order-items.component';
 import {HttpClientModule} from "@angular/common/http";
 import { CategoryItemsComponent } from './componants/category-items/category-items.component';
 import {RouterModule , Routes} from "@angular/router";
 import {CommonModule} from "@angular/common";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { DropdownMenuComponent } from './componants/dropdown-menu/dropdown-menu.component';
 import { SearchOrderComponent } from './componants/search-order/search-order.component';
 import { OrderDetailsComponent } from './componants/order-details/order-details.component';
-
+import {NgbPaginationModule} from "@ng-bootstrap/ng-bootstrap";
 
 
 const routes:Routes =[
@@ -47,8 +45,9 @@ const routes:Routes =[
     HttpClientModule,
     BrowserModule,
     CommonModule,
+    NgbPaginationModule,
     //NgModule()
-    ],
+  ],
   providers: [
 
   ],
